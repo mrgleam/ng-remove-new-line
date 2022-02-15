8b4977a17a9b2066d795a16be9953c738dc33de3
mrgleam <mrgleam@users.noreply.github.com>
2022-02-15 17:05:22 +0000

    Deploying to gh-pages from  @ 40bab0968fd77db67642083182376be6906e6e04 🚀

--- lib/directives/remove-new-line/remove-new-line.module.d.ts
+++
@@ -1,8 +0,0 @@
-import * as i0 from "@angular/core";
-import * as i1 from "./remove-new-line.directive";
-import * as i2 from "@angular/common";
-export declare class RemoveNewLineModule {
-    static ɵfac: i0.ɵɵFactoryDeclaration<RemoveNewLineModule, never>;
-    static ɵmod: i0.ɵɵNgModuleDeclaration<RemoveNewLineModule, [typeof i1.RemoveNewLineDirective], [typeof i2.CommonModule], [typeof i1.RemoveNewLineDirective]>;
-    static ɵinj: i0.ɵɵInjectorDeclaration<RemoveNewLineModule>;
-}
--- public-api.d.ts
+++
@@ -1,2 +0,0 @@
-export * from './lib/directives/remove-new-line/remove-new-line.module';
-export * from './lib/directives/remove-new-line/remove-new-line.directive';
